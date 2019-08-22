db635a0d26704618fa726e70946785a0545971c0
Richard Liu <richard.y.liu@berkeley.edu>
2019-08-22 00:49:37 -0700

    Finished tutorials 5-7

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import Question
+from django.utils import timezone
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+admin.site.register(Question, QuestionAdmin)
